99cfa5d70f1bacea1fa87326bba469ceb88c85c0
keaneozzie <ozziedharma897@gmail.com>
2026-06-12 16:57:35 +0700

    membuat struktur dasar program binary tree dan node class

---
+++ BinarySearchTree_0192.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//membuat class node
+class Node {
+
+    //membuat akses modifier serta mendeklarasi variabel dan object node
+public :
+    string info;
+    Node* leftChild;
+    Node* rightChild;
+
