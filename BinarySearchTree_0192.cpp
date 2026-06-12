18b9b132306519b04e8941ee651b807e611945cb
keaneozzie <ozziedharma897@gmail.com>
2026-06-12 17:03:18 +0700

    menambahkan logika insert ke child kiri dan kanan

--- BinarySearchTree_0192.cpp
+++ BinarySearchTree_0192.cpp
@@ -45,3 +45,15 @@ public :
             ROOT = newNode;
             return;
         }
+
+        if (element < parent->info)
+        {
+            parent->leftChild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightChild = newNode;
+        }
+    }
+
+    
